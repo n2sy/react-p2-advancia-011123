b896e0f57c4df6c3deb68b72b7c97697095c7f95
Nidhal <nidhal@N2SY.local>
2023-11-02 13:29:42 +0100

    add useeffect

--- src/pages/Allfilms.js
+++ src/pages/Allfilms.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FilmItem from "../components/FilmItem";
 
 function Allfilms(props) {
+  const [tabFilms, setTabFilms] = useState([]);
+
+  useEffect(() => {
+    fetch("https://filmstore-409b9-default-rtdb.firebaseio.com/Films.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        for (const key in data) {
+          setTabFilms((previous) => {
+            console.log(previous);
+            return [
+              ...previous,
+              {
+                id: key,
+                ...data[key],
+              },
+            ];
+          });
+        }
+      });
+  }, []);
+
   return (
     <div>
       <ol>
-        {props.list.map((element) => {
+        {tabFilms.map((element) => {
           return <FilmItem key={element.id} film={element}></FilmItem>;
         })}
       </ol>
